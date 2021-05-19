ae833822365f6613e1d2d6eac3d9617734ace2e0
Leire Osés Sánchez <leoses@ucm.es>
2021-05-19 17:44:54 +0200

    Ejercicio 4 práctica 2.2 sin probarlo

--- práctica_2.2/practica2.2/replicacion-chat/Socket.cc
+++ práctica_2.2/practica2.2/replicacion-chat/Socket.cc
@@ -7,6 +7,38 @@ Socket::Socket(const char * address, const char * port):sd(-1)
 {
     //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
     //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+    struct addrinfo hints;
+    struct addrinfo *res;
+
+    //Memoria reservada para el struct con "los filtros" para las conexiones
+    memset((void *)&hints, 0, sizeof(struct addrinfo));
+
+    hints.ai_family = AF_INET;      //IPv4
+    hints.ai_socktype = SOCK_DGRAM; //UDP
+
+    //Cogemos en res las conexiones que cumplen los filtros
+    int rcv = getaddrinfo(address, port, &hints, &res);
+
+    //Gestion de errores
+    if (rcv != 0)
+    {
+        fprintf(stderr, "Error [getaddrinfo]: %s\n", gai_strerror(rcv));
+    }
+
+    sd = socket(res->ai_family, res->ai_socktype, 0);
+
+    //Si no se ha podido crear un socket
+    if (sd == -1)
+    {
+        std::cerr << "Error en la creación de [socket]\n";
+    }
+
+    //Asignamos valor a las variables sa y sa_len
+    sa = *res->ai_addr;
+    sa_len = res->ai_addrlen;
+
+    freeaddrinfo(res);
+
 }
 
 int Socket::recv(Serializable &obj, Socket * &sock)
@@ -36,7 +68,17 @@ int Socket::recv(Serializable &obj, Socket * &sock)
 int Socket::send(Serializable& obj, const Socket& sock)
 {
     //Serializar el objeto
+    obj.to_bin();
+
     //Enviar el objeto binario a sock usando el socket sd
+    ssize_t bytes = sendto(sd,obj.data(),obj.size(),0,&sock.sa, sock.sa_len);
+
+    if ( bytes <= 0 )
+    {
+        return -1;
+    }
+
+    return 0;
 }
 
 bool operator== (const Socket &s1, const Socket &s2)
@@ -44,6 +86,20 @@ bool operator== (const Socket &s1, const Socket &s2)
     //Comparar los campos sin_family, sin_addr.s_addr y sin_port
     //de la estructura sockaddr_in de los Sockets s1 y s2
     //Retornar false si alguno difiere
+
+    //Comparamos sin_family
+    if(s1.sa.sa_family != s2.sa.sa_family) return false;
+
+    struct sockaddr_in *s1_in = (struct sockaddr_in *) &(s1.sa);
+    struct sockaddr_in *s2_in = (struct sockaddr_in *) &(s2.sa);
+
+    //Compararamos sin_addr.s_addr
+    if(s1_in->sin_addr.s_addr != s2_in->sin_addr.s_addr) return false;
+    //Comparamos sin_port
+    if(s1_in->sin_port != s2_in->sin_port) return false;
+
+    //Si llegamos hasta aqui es que son iguales
+    return true;
 };
 
 std::ostream& operator<<(std::ostream& os, const Socket& s)
